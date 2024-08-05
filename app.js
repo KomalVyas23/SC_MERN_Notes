815c1efd26be39badf2917b4d651f48eeb215358
Komal Vyas <shivangi.vyas0804@gmail.com>
2024-08-05 14:28:02 +0530

    ch-2 Mongo DB and CRUD

--- app.js
+++ app.js
@@ -2,9 +2,52 @@ const express = require('express');
 const fs = require('fs');
 const short = require('short-uuid');
 const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 
 dotenv.config();
 const PORT = process.env.PORT || 3300;
+
+/** database connection starts */
+ mongoose.connect(process.env.DB_URL)
+ .then((connection) => {
+    console.log("DB Connected");
+ }).catch((err) => {
+    console.log(err);
+ });
+
+ /** DB Connection End */
+
+ const userSchema = new mongoose.Schema({
+   name: {
+    type: String,
+    required: true,
+   },
+   email: {
+    type: String,
+    required: true,
+    unique: true,
+   },
+   password: {
+    type: String,
+    required: true,
+    minlength: 8,
+   },
+   confirmPassword: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+        validator: function(){
+            return this.password === this.confirmPassword
+        },
+        message: "Password and confirm password should be same"
+    }
+   },
+   createdAt: Date,
+   id: String
+ });
+
+ const User = mongoose.model('User',userSchema);
 
 const app = express();
 const data = fs.readFileSync("./data.json", "utf8");
@@ -17,30 +60,32 @@ app.use((req, res, next) => {
     next();
 });
 
-//Route and a route handler
-app.get('/api/user',(req, res)=>{
+//Routes
+app.get('/api/user', getUser);
+app.post('/api/user', createUser);
+app.get("/api/user/:id", getUserById);
+
+//Routes Handler
+async function getUser (req, res){
     try{
-        let msg = '';
-        if(userData.length === 0){
-            msg = "No user found";
-        }else{
-            msg = "Data found";
-        }
-    
-        res.json({
-            status: 200,
-            data: userData,
-            message: msg,
-        });
+      const userData = await User.find();
+      if(userData === 0){
+        throw new error("No user found")
+      }else{
+        res.status(200).json({
+            message: userData,
+        })
+      }
     }catch(error){
         res.status(500).json({
             message: error.message
         })
     }
-});
+}
 
-app.post('/api/user', (req, res) => {
-    const id = short.generate();
+async function createUser(req, res) {
+    try{
+  const id = short.generate();
     const userDetails = req.body;
     const isEmpty = Object.keys(userDetails).length === 0;
     if(isEmpty){
@@ -49,31 +94,25 @@ app.post('/api/user', (req, res) => {
             message: "Body cannot be empty",
         });
     }else{
-        userDetails.id = id;
-        userData.push(userDetails);
-        fs.writeFile('./data.json', JSON.stringify(userData), (err) => {
-            if(err){
-                console.log(err);
-            }
-            res.json({
-                status: 200,
-                data: userDetails,
-                message: `User created with id ${id}`,
-            });
-        });
+      const user = await User.create(userDetails);
+      res.status(201).json({
+        message: "User created successfully",
+        data: user,
+      });
     }
-});
+    }catch(err){
+        res.status(500).json({
+            status: 500,
+            message: err.message,
+        })
+    }
+}
 
-app.get("/api/user/:id", (req, res) => {
+async function getUserById(req, res){
     try {
       const { id } = req.params;
-      console.log("userid", id);
-      const user = userData.find((user) => user.id === id);
+      const user = await User.findById(id);
       if (!user) {
-        // res.status(404).json({
-        //     status: 404,
-        //     message: "User not found",
-        //    })
         throw new Error("User not found");
       }
       res.json({
@@ -87,20 +126,7 @@ app.get("/api/user/:id", (req, res) => {
         message: err.message,
       });
     }
-  });
-
-//   app.patch('/api/user/:id', (req, res) => {
-//     try{
-//         const { id } = req.params;
-
-
-//     }catch(err){
-//         res.status(500).json({
-//             status: 500,
-//             message: err.message,
-//         })
-//     }
-//   });
+  }
 
 app.listen(PORT,() => {
     console.log(`Server is running at port ${PORT}`);
